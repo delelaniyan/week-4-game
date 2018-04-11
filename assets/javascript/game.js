b0d17ccc8c5bf9250f5f0c0dfbc179449f7d6133
delelaniyan <36045663+delelaniyan@users.noreply.github.com>
2018-04-11 11:22:08 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	colorcrystals = ['assets/images/codingtrainlogo.jpg', 'assets/images/codingtrainsemicolon.jpg', 'assets/images/daddyDdance.jpg', 'assets/images/dannybrokehisarm.jpg'];
+	colorcrystals = ['assets/images/codingtrainlogo.jpg', 'assets/images/codingtrainsemicolon.jpg', 'assets/images/daddyDdance.jpg', 'assets/images/Dannybrokehisarm.jpg'];
 	
 	var wins = 89;
 	var lose = 0;
